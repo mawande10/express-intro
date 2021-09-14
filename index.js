171290889cbdabe2fc0b459a562c65e6e803e67b
Mawande <mawandesikibi.com>
2021-09-15 00:07:15 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let app = express();
+
+//Handlebars Section ...
+const exphbs  = require('express-handlebars');
+
+//Static Resource
+app.use(express.static('public'));
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+app.get("/", function(req, res){
+  res.send("Bill Settings WebApp");
+});
+
+let PORT = process.env.PORT || 3007;
+
+app.listen(PORT, function(){
+  console.log('App starting on port', PORT);
+});
